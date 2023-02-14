837af096fc02196d81ae2056549e4c686957d1a5
5xlm <5xlm@fresnou.org>
2023-02-14 14:13:34 -0800

    Added input1.js

---
+++ input1.js
@@ -0,0 +1,10 @@
+const readline = require('readline-sync')
+
+let answer = readline.question("Wanna hear a joke?\nYour answer:");
+console.log(answer);
+if (answer =="yes") {
+    console.log('Why do we tell actors to break a leg, because every play has a cast.');
+}
+
+
+//2
